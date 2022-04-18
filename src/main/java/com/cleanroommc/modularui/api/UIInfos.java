df5ec7a59910fb5ae1fd0453c497ca5c5529fe4d
brachy84 <brachy84@gmail.com>
2022-04-18 07:38:52 +0200

    client screen helper

--- src/main/java/com/cleanroommc/modularui/api/UIInfos.java
+++ src/main/java/com/cleanroommc/modularui/api/UIInfos.java
@@ -1,15 +1,24 @@
 package com.cleanroommc.modularui.api;
 
+import com.cleanroommc.modularui.ModularUI;
 import com.cleanroommc.modularui.api.screen.ITileWithModularUI;
-import com.cleanroommc.modularui.common.builder.UIBuilder;
-import com.cleanroommc.modularui.common.builder.UIInfo;
 import com.cleanroommc.modularui.api.screen.ModularUIContext;
 import com.cleanroommc.modularui.api.screen.ModularWindow;
 import com.cleanroommc.modularui.api.screen.UIBuildContext;
+import com.cleanroommc.modularui.common.builder.UIBuilder;
+import com.cleanroommc.modularui.common.builder.UIInfo;
 import com.cleanroommc.modularui.common.internal.wrapper.ModularGui;
 import com.cleanroommc.modularui.common.internal.wrapper.ModularUIContainer;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.function.Function;
 
 public class UIInfos {
 
@@ -33,4 +42,16 @@ public class UIInfos {
                 return null;
             })
             .build();
+
+    @SideOnly(Side.CLIENT)
+    public static void openClientUI(EntityPlayer player, Function<UIBuildContext, ModularWindow> uiCreator) {
+        if (!(player instanceof EntityPlayerSP)) {
+            ModularUI.LOGGER.info("Tried opening client ui on server!");
+            return;
+        }
+        UIBuildContext buildContext = new UIBuildContext(player);
+        ModularWindow window = uiCreator.apply(buildContext);
+        GuiScreen screen = new ModularGui(new ModularUIContainer(new ModularUIContext(buildContext), window));
+        FMLCommonHandler.instance().showGuiScreen(screen);
+    }
 }
