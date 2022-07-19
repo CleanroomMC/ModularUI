c59fea553a05526e1f8a0a370514a44514de5f64
brachy84 <brachy84@gmail.com>
2022-07-19 17:08:47 +0200

    ui keybind helpers

---
+++ src/main/java/com/cleanroommc/modularui/common/keybind/KeyBindHandler.java
@@ -0,0 +1,75 @@
+package com.cleanroommc.modularui.common.keybind;
+
+import com.cleanroommc.modularui.ModularUI;
+import com.cleanroommc.modularui.api.KeyBindAPI;
+import com.cleanroommc.modularui.core.mixin.KeyBindAccess;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.client.settings.KeyBindingMap;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+
+import java.util.Collection;
+
+@Mod.EventBusSubscriber(modid = ModularUI.ID, value = Side.CLIENT)
+public class KeyBindHandler {
+
+    private static void checkKeyState(int key, boolean state) {
+        if (key != 0) {
+            // imitates KeyBinding.setKeyBindState()
+            for (KeyBinding keyBinding : getKeyBindingMap().lookupAll(key)) {
+                if (KeyBindAPI.doForceCheckKeyBind(keyBinding)) {
+                    ((KeyBindAccess) keyBinding).setPressed(state);
+                }
+            }
+            // imitates KeyBinding.onTick()
+            if (state) {
+                KeyBinding keyBinding = getKeyBindingMap().lookupActive(key);
+                if (keyBinding != null) {
+                    if (KeyBindAPI.doForceCheckKeyBind(keyBinding)) {
+                        incrementPressTime(keyBinding);
+                    }
+
+                    Collection<KeyBinding> compatibles = KeyBindAPI.getCompatibles(keyBinding);
+                    if (compatibles.isEmpty()) return;
+                    for (KeyBinding keyBinding1 : compatibles) {
+                        if (keyBinding1.isActiveAndMatches(key) && KeyBindAPI.doForceCheckKeyBind(keyBinding1)) {
+                            incrementPressTime(keyBinding1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void onGuiKeyInput(GuiScreenEvent.KeyboardInputEvent.Pre event) {
+        if (!event.getGui().isFocused()) {
+            int key = Keyboard.getEventKey();
+            boolean state = Keyboard.getEventKeyState();
+            checkKeyState(key, state);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void onMouseInput(GuiScreenEvent.MouseInputEvent.Pre event) {
+        if (!event.getGui().isFocused()) {
+            int key = Mouse.getEventButton() - 100;
+            boolean state = Mouse.getEventButtonState();
+            checkKeyState(key, state);
+        }
+    }
+
+    public static KeyBindingMap getKeyBindingMap() {
+        return ((KeyBindAccess) Minecraft.getMinecraft().gameSettings.keyBindPickBlock).getHASH();
+    }
+
+    public static void incrementPressTime(KeyBinding keyBinding) {
+        ((KeyBindAccess) keyBinding).setPressTime(((KeyBindAccess) keyBinding).getPressTime() + 1);
+    }
+}
---
+++ src/main/java/com/cleanroommc/modularui/core/mixin/KeyBindAccess.java
@@ -0,0 +1,22 @@
+package com.cleanroommc.modularui.core.mixin;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.settings.KeyBindingMap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(KeyBinding.class)
+public interface KeyBindAccess {
+
+    @Accessor
+    KeyBindingMap getHASH();
+
+    @Accessor
+    void setPressed(boolean pressed);
+
+    @Accessor
+    int getPressTime();
+
+    @Accessor
+    void setPressTime(int time);
+}
