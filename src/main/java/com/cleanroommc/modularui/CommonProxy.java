82d3f4bbdc06bce51c6f3dab0046c4356c3871f1
brachy84 <brachy84@gmail.com>
2022-07-22 19:27:24 +0200

    save config on change

--- src/main/java/com/cleanroommc/modularui/CommonProxy.java
+++ src/main/java/com/cleanroommc/modularui/CommonProxy.java
@@ -6,7 +6,10 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -38,5 +41,12 @@ public class CommonProxy {
     public static void registerItems(RegistryEvent.Register<Item> event) {
         IForgeRegistry<Item> registry = event.getRegistry();
         registry.register(testItemBlock);
+    }
+
+    @SubscribeEvent
+    public static void onConfigChange(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if (event.getModID().equals(ModularUI.ID)) {
+            ConfigManager.sync(ModularUI.ID, Config.Type.INSTANCE);
+        }
     }
 }
