ee9d2fdd206ef9decdd97da3815a9d5aeaff2321
brachy84 <45517902+brachy84@users.noreply.github.com>
2023-05-17 19:37:11 +0200

    Theme improvements (#15)
    * improve theme json parsing
    
    * seperate theme api and internals & java doc
    
    * add override theme property
    
    * fixes

--- src/main/java/com/cleanroommc/modularui/api/ITheme.java
+++ src/main/java/com/cleanroommc/modularui/api/ITheme.java
@@ -1,6 +1,8 @@
 package com.cleanroommc.modularui.api;
 
-import com.cleanroommc.modularui.theme.*;
+import com.cleanroommc.modularui.theme.WidgetSlotTheme;
+import com.cleanroommc.modularui.theme.WidgetTextFieldTheme;
+import com.cleanroommc.modularui.theme.WidgetTheme;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -14,7 +16,7 @@ public interface ITheme {
      * @return the master default theme.
      */
     static ITheme getDefault() {
-        return Theme.DEFAULT_DEFAULT;
+        return IThemeApi.get().getDefaultTheme();
     }
 
     /**
@@ -22,39 +24,7 @@ public interface ITheme {
      * @return theme with given id
      */
     static ITheme get(String id) {
-        return ThemeManager.get(id);
-    }
-
-    /**
-     * If you have a custom widget type and want a custom theme for the type you can register a parser here.
-     *
-     * @param id           the id of the widget theme. This how json finds this parser.
-     * @param defaultTheme this will be used for {@link Theme#DEFAULT_DEFAULT}.
-     * @param function     a function that creates an instance of the widget theme with the parent theme and a json.
-     */
-    static void registerWidgetTheme(String id, WidgetTheme defaultTheme, WidgetThemeParser function) {
-        ThemeManager.registerWidgetTheme(id, defaultTheme, function);
-    }
-
-    /**
-     * Register a default theme for a screen id. Can also just be the mod from many screens.
-     *
-     * @param screenId screen id or mod.
-     * @param theme    theme
-     */
-    static void registerDefaultScreenTheme(String screenId, ITheme theme) {
-        ThemeManager.registerDefaultTheme(screenId, theme.getId());
-    }
-
-    /**
-     * Register a default theme for a screen id.
-     *
-     * @param mod   mod id of screen
-     * @param id    screen id without mod
-     * @param theme theme
-     */
-    static void registerDefaultScreenTheme(String mod, String id, ITheme theme) {
-        ThemeManager.registerDefaultTheme(mod + ":" + id, theme.getId());
+        return IThemeApi.get().getTheme(id);
     }
 
     String getId();
@@ -74,5 +44,4 @@ public interface ITheme {
     WidgetTextFieldTheme getTextFieldTheme();
 
     WidgetTheme getWidgetTheme(String id);
-
 }
