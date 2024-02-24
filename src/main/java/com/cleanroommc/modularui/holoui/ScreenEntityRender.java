95f50489d3859e44297f7f68b58acdbcda27fc08
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-02-24 16:54:24 -0700

    trig fuckery

--- src/main/java/com/cleanroommc/modularui/holoui/ScreenEntityRender.java
+++ src/main/java/com/cleanroommc/modularui/holoui/ScreenEntityRender.java
@@ -1,5 +1,6 @@
 package com.cleanroommc.modularui.holoui;
 
+import com.cleanroommc.modularui.ModularUI;
 import com.cleanroommc.modularui.screen.GuiContainerWrapper;
 
 import net.minecraft.client.Minecraft;
@@ -9,9 +10,15 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
+
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import javax.vecmath.Vector2d;
 
 /**
  * Highly experimental
@@ -37,13 +44,57 @@ public class ScreenEntityRender extends Render<HoloScreenEntity> {
         Plane3D plane3D = entity.getPlane3D();
         GlStateManager.pushMatrix();
         GlStateManager.translate(x, y, z);
+        EntityPlayer player = Minecraft.getMinecraft().player;
         if (entity.getOrientation() == ScreenOrientation.TO_PLAYER) {
-            EntityPlayer player = Minecraft.getMinecraft().player;
             plane3D.transform(player.getPositionVector(), entity.getPositionVector(), player.getLookVec());
         } else {
             plane3D.transform();
         }
-        screenWrapper.drawScreen(0, 0, partialTicks);
+        var mouse = calculateMousePos(player, entity, player.getLookVec());
+        screenWrapper.drawScreen(mouse.getX(), mouse.getY(), partialTicks);
         GlStateManager.popMatrix();
+    }
+
+    private Vec3i calculateMousePos(EntityPlayer player, HoloScreenEntity entity, Vec3d looking) {
+        var holoPos = entity.getPositionVector();
+        var pos = player.getPositionVector().add(0, player.getEyeHeight(), 0);
+
+        var plane = entity.getPlane3D();
+        var planeRot = plane.getRotation();
+
+        // get the difference of the player's eye position and holo position
+        // rotate diff based on plane rotation
+        double worldAngle = calculateHorizontalAngle(holoPos);
+        var posR = pos.rotateYaw((float) (planeRot.y - worldAngle));
+        var holoR = holoPos.rotateYaw((float) (planeRot.y - worldAngle));
+
+        // x should be the left-right offset from the player to the holo screen
+        // z should be the distance from the player to the holo screen's plane
+        var diff = holoR.subtract(posR);
+
+        // rotate to make x zero
+        var lookRot = looking
+                .rotateYaw((float) (planeRot.y - worldAngle))
+                .rotatePitch((float) planeRot.x);
+
+        // the x, y of look rot should be the mouse pos if scaled by looRot z
+        // the scale factor should be the distance from the player to the plane by the z component of lookRot
+        double sf = diff.z / lookRot.z;
+        double mX = ((lookRot.x * sf) - diff.x) * 16;
+        double mY = ((lookRot.y * -sf) + diff.y) * 16;
+        mY += plane.getHeight() / 2;
+        mX += plane.getWidth() / 2;
+
+        return new Vec3i(mX, mY, 0);
+    }
+
+    private double calculateHorizontalAngle(Vec3d vec) {
+        // x is opposite, z is adjacent, theta = atan(x/z)
+        double a3 = Math.atan(vec.x / vec.z);
+        if (vec.z < 0) {
+            // if z is negative, the angle returned by atan has to be offset by PI
+            a3 += vec.x < 0 ? -Math.PI : Math.PI;
+        }
+        return a3;
     }
 }
