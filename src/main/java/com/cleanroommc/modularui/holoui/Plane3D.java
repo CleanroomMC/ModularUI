b460ce50bebca0b85025ae01700aee6984910ef6
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-02-24 01:13:19 -0700

    fix in world transforms

--- src/main/java/com/cleanroommc/modularui/holoui/Plane3D.java
+++ src/main/java/com/cleanroommc/modularui/holoui/Plane3D.java
@@ -6,9 +6,10 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 
+import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.ApiStatus;
 import org.lwjgl.util.vector.Matrix4f;
-import org.lwjgl.util.vector.Quaternion;
+import org.lwjgl.util.vector.Vector3f;
 
 /**
  * Highly experimental
@@ -19,10 +20,14 @@ public class Plane3D {
     private float w = 480, h = 270;
     private float scale = 1f;
     private float aX = 0.5f, aY = 0.5f;
-    private float nX = 0, nY = 0, nZ = 1;
-    private final Quaternion facer = new Quaternion();
+    private Vec3d normal = Direction.ORIGIN.asVec3d();
 
-    public void transformRectangle() {
+
+    public void transform() {
+        transform(this.normal, Direction.ORIGIN.asVec3d(), this.normal.scale(-1));
+    }
+
+    public void transform(Vec3d target, Vec3d orig, Vec3d looking) {
         // translate to anchor
         GlStateManager.translate(-this.w * this.aX, -this.h * this.aY, 0);
         // translate for scale and rotation
@@ -32,19 +37,22 @@ public class Plane3D {
         // rotate 180 deg
         GlStateManager.rotate(180, 0, 0, 1);
         // apply facing direction
-        if (this.nX != 0 || this.nY != 0 || this.nZ != 1) {
-            Matrix4f rotation = new Matrix4f();
-            rotation.m00 = -this.nZ + (this.nY * this.nY * (1 + this.nZ)) / (this.nX * this.nX + this.nY * this.nY);
-            rotation.m10 = -(this.nX * this.nY * (1 + this.nZ)) / (this.nX * this.nX + this.nY * this.nY);
-            rotation.m20 = this.nX;
-            rotation.m01 = -(this.nX * this.nY * (1 + this.nZ)) / (this.nX * this.nX + this.nY * this.nY);
-            rotation.m11 = -this.nZ + (this.nX * this.nX * (1 + this.nZ)) / (this.nX * this.nX + this.nY * this.nY);
-            rotation.m21 = this.nY;
-            rotation.m02 = -this.nX;
-            rotation.m12 = -this.nY;
-            rotation.m22 = -this.nZ;
-            GuiUtils.applyTransformationMatrix(rotation);
-        }
+        Vec3d diff = orig.subtract(target);
+        double yaw = Math.atan(diff.z / diff.x);
+        if (diff.x < 0) yaw += (Math.PI / 2);
+        else yaw -= (Math.PI / 2);
+
+        Vec3d vec = new Vec3d(looking.x, 0, looking.z);
+        double pitch = Math.atan(looking.y / vec.length());
+//        if (orig.x < 0) pitch -= (Math.PI / 2);
+
+        Matrix4f mYaw = new Matrix4f()
+                .rotate((float) yaw, Direction.UP.asVector3f());
+        Matrix4f mPitch = new Matrix4f()
+                .rotate((float) pitch, Direction.LEFT.asVector3f());
+
+        GuiUtils.applyTransformationMatrix(mYaw);
+        GuiUtils.applyTransformationMatrix(mPitch);
         // un-translate for scale and rotation
         GlStateManager.translate(-(this.w / 2f), -(this.h / 2f), 0);
     }
@@ -67,24 +75,7 @@ public class Plane3D {
     }
 
     public void setNormal(float x, float y, float z) {
-        float square = x * x + y * y + z * z;
-        if (square != 1) {
-            float factor = (float) Math.sqrt(square);
-            x /= factor;
-            y /= factor;
-            z /= factor;
-        }
-        this.nX = x;
-        this.nY = y;
-        this.nZ = z;
-    }
-
-    public void setTarget(Entity entity) {
-        BlockPos pos = entity.getPosition();
-        this.facer.x = pos.getX();
-        this.facer.y = pos.getY();
-        this.facer.z = pos.getZ();
-        this.facer.w = 1;
+        this.normal = new Vec3d(x, y, z).normalize();
     }
 
     public void setAnchor(float x, float y) {
@@ -106,5 +97,30 @@ public class Plane3D {
 
     public float getScale() {
         return this.scale;
+    }
+
+    private enum Direction {
+        ORIGIN(0, 0, 0),
+        LEFT(1, 0, 0),
+        UP(0, 1, 0),
+        NORTH(0, 0, 1),
+        RIGHT(-1, 0, 0),
+        DOWN(0, -1, 0),
+        SOUTH(0, 0, -1);
+
+        private final Vector3f vector3f;
+        private final Vec3d vec3d;
+        Direction(float x, float y, float z) {
+            vector3f = new Vector3f(x, y, z);
+            vec3d = new Vec3d(x, y, z);
+        }
+
+        Vector3f asVector3f() {
+            return vector3f;
+        }
+
+        Vec3d asVec3d() {
+            return vec3d;
+        }
     }
 }
