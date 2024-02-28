2bace86a98151ae1955c05aa386d39c53eb33aab
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-02-27 17:55:08 -0700

    update screen and animator handle mouse input add method to check if within a screen

--- src/main/java/com/cleanroommc/modularui/holoui/ScreenEntityRender.java
+++ src/main/java/com/cleanroommc/modularui/holoui/ScreenEntityRender.java
@@ -1,30 +1,32 @@
 package com.cleanroommc.modularui.holoui;
 
-import com.cleanroommc.modularui.ModularUI;
 import com.cleanroommc.modularui.screen.GuiScreenWrapper;
+import com.cleanroommc.modularui.utils.Animator;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.crash.CrashReport;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ReportedException;
 import net.minecraft.util.ResourceLocation;
-
-import net.minecraft.util.math.Vec2f;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.vecmath.Vector2d;
+import java.util.Map;
 
 /**
  * Highly experimental
  */
 @ApiStatus.Experimental
 public class ScreenEntityRender extends Render<HoloScreenEntity> {
+    private static final Map<EntityPlayer, GuiScreenWrapper> lookingPlayers = new Object2ObjectOpenHashMap<>();
 
     public ScreenEntityRender(RenderManager renderManager) {
         super(renderManager);
@@ -50,14 +52,33 @@ public class ScreenEntityRender extends Render<HoloScreenEntity> {
         } else {
             plane3D.transform();
         }
-        var mouse = calculateMousePos(player, entity, player.getLookVec());
+        var mouse = calculateMousePos(player.getPositionVector().add(0, player.getEyeHeight(), 0), entity, player.getLookVec());
         screenWrapper.drawScreen(mouse.getX(), mouse.getY(), partialTicks);
+        screenWrapper.getScreen().onFrameUpdate();
+        Animator.advance();
+        if (withinScreen(mouse, entity.getPlane3D()) && !lookingPlayers.containsKey(player)) {
+            lookingPlayers.put(player, screenWrapper);
+        } else if (!withinScreen(mouse, entity.getPlane3D())) {
+            lookingPlayers.remove(player);
+        }
         GlStateManager.popMatrix();
     }
 
-    private Vec3i calculateMousePos(EntityPlayer player, HoloScreenEntity entity, Vec3d looking) {
+    public static void clickScreen(EntityPlayer player) {
+        if (lookingPlayers.containsKey(player)) {
+            try {
+                lookingPlayers.get(player).handleMouseInput();
+            } catch (Throwable throwable1) {
+                CrashReport c = CrashReport.makeCrashReport(throwable1, "Updating screen events");
+                c.makeCategory("Affected screen")
+                    .addDetail("Screen name", () -> lookingPlayers.get(player).getClass().getCanonicalName());
+                throw new ReportedException(c);
+            }
+        }
+    }
+
+    private static Vec3i calculateMousePos(Vec3d player, HoloScreenEntity entity, Vec3d looking) {
         var holoPos = entity.getPositionVector();
-        var pos = player.getPositionVector().add(0, player.getEyeHeight(), 0);
 
         var plane = entity.getPlane3D();
         var planeRot = plane.getRotation();
@@ -65,7 +86,7 @@ public class ScreenEntityRender extends Render<HoloScreenEntity> {
         // get the difference of the player's eye position and holo position
         // rotate diff based on plane rotation
         double worldAngle = calculateHorizontalAngle(holoPos);
-        var posR = pos.rotateYaw((float) (planeRot.y - worldAngle));
+        var posR = player.rotateYaw((float) (planeRot.y - worldAngle));
         var holoR = holoPos.rotateYaw((float) (planeRot.y - worldAngle));
 
         // x should be the left-right offset from the player to the holo screen
@@ -89,7 +110,12 @@ public class ScreenEntityRender extends Render<HoloScreenEntity> {
         return new Vec3i(mX, mY, 0);
     }
 
-    private double calculateHorizontalAngle(Vec3d vec) {
+    private static boolean withinScreen(Vec3i mousePos, Plane3D plane) {
+        return mousePos.getX() > 0 && mousePos.getX() < plane.getWidth() &&
+                mousePos.getY() > 0 && mousePos.getY() < plane.getHeight();
+    }
+
+    private static double calculateHorizontalAngle(Vec3d vec) {
         // x is opposite, z is adjacent, theta = atan(x/z)
         double a3 = Math.atan(vec.x / vec.z);
         if (vec.z < 0) {
