3cddba7dc59e5fa409a6bbdb24b411d172fa5af4
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-07-23 11:27:17 -0700

    accound for potential server-side crash

--- src/main/java/com/cleanroommc/modularui/screen/ModularContainer.java
+++ src/main/java/com/cleanroommc/modularui/screen/ModularContainer.java
@@ -19,7 +19,7 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.Optional;
+import net.minecraftforge.fml.common.Optional.Interface;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -30,7 +30,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
-@Optional.Interface(modid = ModularUI.BOGO_SORT, iface = "com.cleanroommc.bogosorter.api.ISortableContainer")
+@Interface(modid = ModularUI.BOGO_SORT, iface = "com.cleanroommc.bogosorter.api.ISortableContainer")
 public class ModularContainer extends Container implements ISortableContainer {
 
     public static ModularContainer getCurrent(EntityPlayer player) {
@@ -46,7 +46,8 @@ public class ModularContainer extends Container implements ISortableContainer {
     private final List<ModularSlot> slots = new ArrayList<>();
     private final List<ModularSlot> shiftClickSlots = new ArrayList<>();
     private ContainerCustomizer containerCustomizer;
-    private ModularScreen modularScreen = null;
+
+    private Optional<?> optionalScreen = Optional.empty();
 
     public ModularContainer(EntityPlayer player, PanelSyncManager panelSyncManager, String mainPanelName) {
         this.player = player;
@@ -68,13 +69,15 @@ public class ModularContainer extends Container implements ISortableContainer {
         this.containerCustomizer.initialize(this);
     }
 
+    @SideOnly(Side.CLIENT)
     public void setScreen(ModularScreen screen) {
-        this.modularScreen = screen;
+        this.optionalScreen = Optional.ofNullable(screen);
     }
 
     @Nullable
+    @SideOnly(Side.CLIENT)
     public ModularScreen getScreen() {
-        return modularScreen;
+        return (ModularScreen) optionalScreen.orElse(null);
     }
 
     public ContainerAccessor acc() {
