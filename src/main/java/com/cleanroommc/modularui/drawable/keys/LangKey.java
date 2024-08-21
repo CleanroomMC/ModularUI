1ed3151605f56dcde86a79a094a38cc6e03fc33c
miozune <miozune@gmail.com>
2024-08-22 01:00:17 +0900

    Make LangKey accept supplier

--- src/main/java/com/cleanroommc/modularui/drawable/keys/LangKey.java
+++ src/main/java/com/cleanroommc/modularui/drawable/keys/LangKey.java
@@ -9,38 +9,52 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
+import java.util.function.Supplier;
 
 public class LangKey implements IKey {
 
-    private final String key;
-    private final Object[] args;
+    private final Supplier<String> keySupplier;
+    private final Supplier<Object[]> argsSupplier;
     private String string;
     private long time = 0;
 
     public LangKey(@NotNull String key) {
-        this(key, null);
+        this(key, () -> null);
     }
 
     public LangKey(@NotNull String key, @Nullable Object[] args) {
-        this.key = Objects.requireNonNull(key);
-        this.args = args == null || args.length == 0 ? null : args;
+        this(() -> Objects.requireNonNull(key), () -> args == null || args.length == 0 ? null : args);
     }
 
-    public String getKey() {
-        return this.key;
+    public LangKey(@NotNull String key, @NotNull Supplier<Object[]> argsSupplier) {
+        this(() -> Objects.requireNonNull(key), argsSupplier);
     }
 
-    public Object[] getArgs() {
-        return this.args;
+    public LangKey(@NotNull Supplier<String> keySupplier) {
+        this(keySupplier, () -> null);
+    }
+
+    public LangKey(@NotNull Supplier<String> keySupplier, @NotNull Supplier<Object[]> argsSupplier) {
+        this.keySupplier = Objects.requireNonNull(keySupplier);
+        this.argsSupplier = Objects.requireNonNull(argsSupplier);
+    }
+
+    public Supplier<String> getKeySupplier() {
+        return keySupplier;
+    }
+
+    public Supplier<Object[]> getArgsSupplier() {
+        return argsSupplier;
     }
 
     @Override
     public String get() {
-        if (this.string == null || (this.args != null && this.time != ClientEventHandler.getTicks())) {
-            this.time = ClientEventHandler.getTicks();
-            this.string = I18n.format(this.key, this.args);
+        if (this.time == ClientEventHandler.getTicks()) {
+            return this.string;
         }
-        return this.string;
+        this.time = ClientEventHandler.getTicks();
+        this.string = I18n.format(Objects.requireNonNull(this.keySupplier.get()), this.argsSupplier.get());
+        return string;
     }
 
     @Override
