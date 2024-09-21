557671782484447fd47c2ca99acce0295fd7277b
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-09-21 14:14:07 -0700

    yeet ClientEventHandler and move it into ClientScreenHandler

--- src/main/java/com/cleanroommc/modularui/ClientProxy.java
+++ src/main/java/com/cleanroommc/modularui/ClientProxy.java
@@ -32,7 +32,6 @@ public class ClientProxy extends CommonProxy {
     void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
 
-        MinecraftForge.EVENT_BUS.register(ClientEventHandler.class);
         MinecraftForge.EVENT_BUS.register(ClientScreenHandler.class);
         MinecraftForge.EVENT_BUS.register(OverlayManager.class);
         MinecraftForge.EVENT_BUS.register(KeyBindHandler.class);
