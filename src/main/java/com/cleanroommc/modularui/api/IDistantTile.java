0af8802ccca2125d544e47dd2a519c0ed9e59647
hohserg1 <hohserg1@yandex.ru>
2024-12-23 19:13:54 +0300

    added javadoc to IDistantTile

--- src/main/java/com/cleanroommc/modularui/api/IDistantTile.java
+++ src/main/java/com/cleanroommc/modularui/api/IDistantTile.java
@@ -1,5 +1,8 @@
 package com.cleanroommc.modularui.api;
-
+/**
+ * An interface to implement on {@link net.minecraft.tileentity.TileEntity}.
+ * Allow changing max distance between tile and player (default 8 blocks).
+ */
 public interface IDistantTile {
     double getInteractionDistance();
 }
