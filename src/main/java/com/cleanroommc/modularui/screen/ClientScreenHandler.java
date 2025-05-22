2a1a0f413530faff6100478cd0bddac0916b1534
brachy84 <brachy84@gmail.com>
2025-05-22 19:56:15 +0200

    call update in tick end

--- src/main/java/com/cleanroommc/modularui/screen/ClientScreenHandler.java
+++ src/main/java/com/cleanroommc/modularui/screen/ClientScreenHandler.java
@@ -193,7 +193,7 @@ public class ClientScreenHandler {
 
     @SubscribeEvent
     public static void onTick(TickEvent.ClientTickEvent event) {
-        if (event.phase == TickEvent.Phase.START) {
+        if (event.phase == TickEvent.Phase.END) {
             OverlayStack.onTick();
             defaultContext.tick();
             if (checkGui()) {
