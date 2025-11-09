687565a816587c96119e2f3c37bda52cc41df2fd
brachy84 <brachy84@gmail.com>
2025-11-09 11:57:33 +0100

    string value impl for big int and big decimal sync value

--- src/main/java/com/cleanroommc/modularui/value/sync/BigDecimalSyncValue.java
+++ src/main/java/com/cleanroommc/modularui/value/sync/BigDecimalSyncValue.java
@@ -1,18 +1,29 @@
 package com.cleanroommc.modularui.value.sync;
 
+import com.cleanroommc.modularui.api.value.IStringValue;
+import com.cleanroommc.modularui.utils.ICopy;
 import com.cleanroommc.modularui.utils.serialization.ByteBufAdapters;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public class BigDecimalSyncValue extends GenericSyncValue<BigDecimal> {
+public class BigDecimalSyncValue extends GenericSyncValue<BigDecimal> implements IStringValue<BigDecimal> {
 
     public BigDecimalSyncValue(@NotNull Supplier<BigDecimal> getter, @Nullable Consumer<BigDecimal> setter) {
-        super(getter, setter, ByteBufAdapters.BIG_DECIMAL, v -> new BigDecimal(v.unscaledValue(), v.scale()));
+        super(getter, setter, ByteBufAdapters.BIG_DECIMAL, ICopy.immutable());
+    }
+
+    @Override
+    public String getStringValue() {
+        return getValue().toString();
+    }
+
+    @Override
+    public void setStringValue(String val) {
+        setValue(new BigDecimal(val));
     }
 }
--- src/main/java/com/cleanroommc/modularui/value/sync/BigIntSyncValue.java
+++ src/main/java/com/cleanroommc/modularui/value/sync/BigIntSyncValue.java
@@ -1,5 +1,7 @@
 package com.cleanroommc.modularui.value.sync;
 
+import com.cleanroommc.modularui.api.value.IStringValue;
+import com.cleanroommc.modularui.utils.ICopy;
 import com.cleanroommc.modularui.utils.serialization.ByteBufAdapters;
 
 import org.jetbrains.annotations.NotNull;
@@ -9,9 +11,19 @@ import java.math.BigInteger;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public class BigIntSyncValue extends GenericSyncValue<BigInteger> {
+public class BigIntSyncValue extends GenericSyncValue<BigInteger> implements IStringValue<BigInteger> {
 
     public BigIntSyncValue(@NotNull Supplier<BigInteger> getter, @Nullable Consumer<BigInteger> setter) {
-        super(getter, setter, ByteBufAdapters.BIG_INT, v -> new BigInteger(v.toByteArray()));
+        super(getter, setter, ByteBufAdapters.BIG_INT, ICopy.immutable());
+    }
+
+    @Override
+    public String getStringValue() {
+        return getValue().toString();
+    }
+
+    @Override
+    public void setStringValue(String val) {
+        setValue(new BigInteger(val));
     }
 }
