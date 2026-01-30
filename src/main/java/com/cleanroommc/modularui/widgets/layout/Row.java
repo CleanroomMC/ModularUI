123e3e56585a5bd309b494640532cfa294b2ff92
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2026-01-30 13:43:53 -0700

    add two helper methods to Flow deprecate row and column classes

--- src/main/java/com/cleanroommc/modularui/widgets/layout/Row.java
+++ src/main/java/com/cleanroommc/modularui/widgets/layout/Row.java
@@ -2,6 +2,10 @@ package com.cleanroommc.modularui.widgets.layout;
 
 import com.cleanroommc.modularui.api.GuiAxis;
 
+import org.jetbrains.annotations.ApiStatus;
+
+@Deprecated
+@ApiStatus.ScheduledForRemoval(inVersion = "3.2")
 public class Row extends Flow {
 
     public Row() {
