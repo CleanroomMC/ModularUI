5a3a9698b4b8a99898fa7122945e4d3f91704b91
brachy84 <brachy84@gmail.com>
2026-02-06 00:46:05 +0100

    log exception in receivePacket()

--- src/main/java/com/cleanroommc/modularui/network/ModularNetworkSide.java
+++ src/main/java/com/cleanroommc/modularui/network/ModularNetworkSide.java
@@ -76,7 +76,8 @@ public abstract class ModularNetworkSide {
             int id = packet.action ? 0 : packet.packet.readVarInt();
             msm.receiveWidgetUpdate(packet.panel, packet.key, packet.action, id, packet.packet);
         } catch (IndexOutOfBoundsException e) {
-            ModularUI.LOGGER.error("Failed to read packet for sync handler {} in panel {}", packet.key, packet.panel);
+            ModularUI.LOGGER.error("Failed to read packet for sync handler {} in panel {}. Exception: ", packet.key, packet.panel);
+            ModularUI.LOGGER.catching(e);
         } catch (IOException e) {
             ModularUI.LOGGER.throwing(e);
         }
