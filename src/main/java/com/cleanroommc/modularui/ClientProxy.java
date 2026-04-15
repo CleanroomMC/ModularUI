GTNewHorizons/ModularUI2

Split container stack to each player (#111)
Co-authored-by: Martin Robertz <dream-master@gmx.net>

src/main/java/com/cleanroommc/modularui/ClientProxy.java
@@ -8,6 +8,7 @@ import com.cleanroommc.modularui.factory.GuiFactories;
 import com.cleanroommc.modularui.factory.inventory.InventoryTypes;
 import com.cleanroommc.modularui.holoui.HoloScreenEntity;
 import com.cleanroommc.modularui.holoui.ScreenEntityRender;
+import com.cleanroommc.modularui.network.ModularNetwork;
 import com.cleanroommc.modularui.screen.ClientScreenHandler;
 import com.cleanroommc.modularui.test.TestItem;
 import com.cleanroommc.modularui.theme.ThemeManager;
@@ -21,6 +22,7 @@ import net.minecraft.util.Timer;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.FMLCommonHandler;
@@ -184,6 +186,16 @@ public class ClientProxy extends CommonProxy {
                 }
                 return false;
             });
+        }
+    }
+
+    @SubscribeEvent
+    public void onUnloadWorld(WorldEvent.Unload event) {
+        ModularNetwork.CLIENT.onPlayerLeave(Minecraft.getMinecraft().thePlayer);
+
+        if (Minecraft.getMinecraft().isIntegratedServerRunning()) {
+            // we need to handle single player here, since PlayerLoggedOutEvent is not triggered for some reason
+            ModularNetwork.SERVER.onPlayerLeave(Minecraft.getMinecraft().thePlayer);
         }
     }
 

src/main/java/com/cleanroommc/modularui/CommonProxy.java
@@ -4,7 +4,9 @@ import com.cleanroommc.modularui.factory.GuiFactories;
 import com.cleanroommc.modularui.factory.GuiManager;
 import com.cleanroommc.modularui.factory.inventory.InventoryTypes;
 import com.cleanroommc.modularui.holoui.HoloScreenEntity;
+import com.cleanroommc.modularui.network.ModularNetwork;
 import com.cleanroommc.modularui.network.NetworkHandler;
+import com.cleanroommc.modularui.network.NetworkUtils;
 import com.cleanroommc.modularui.screen.ModularContainer;
 import com.cleanroommc.modularui.test.ItemEditorGui;
 import com.cleanroommc.modularui.test.TestEventHandler;
@@ -16,6 +18,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.relauncher.Side;
@@ -66,6 +69,13 @@ public class CommonProxy {
     public void onTick(TickEvent.PlayerTickEvent event) {
         if (event.player.openContainer instanceof ModularContainer container) {
             container.onUpdate();
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerLeave(PlayerEvent.PlayerLoggedOutEvent event) {
+        if (!NetworkUtils.isDedicatedClient()) {
+            ModularNetwork.SERVER.onPlayerLeave(event.player);
         }
     }
 }

src/main/java/com/cleanroommc/modularui/network/packets/PacketSyncHandler.java
@@ -4,6 +4,7 @@ import com.cleanroommc.modularui.network.IPacket;
 import com.cleanroommc.modularui.network.ModularNetwork;
 import com.cleanroommc.modularui.network.NetworkUtils;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.PacketBuffer;
@@ -53,13 +54,13 @@ public class PacketSyncHandler implements IPacket {
     @SideOnly(Side.CLIENT)
     @Override
     public @Nullable IPacket executeClient(NetHandlerPlayClient handler) {
-        ModularNetwork.CLIENT.receivePacket(this);
+        ModularNetwork.CLIENT.receivePacket(Minecraft.getMinecraft().thePlayer, this);
         return null;
     }
 
     @Override
     public @Nullable IPacket executeServer(NetHandlerPlayServer handler) {
-        ModularNetwork.SERVER.receivePacket(this);
+        ModularNetwork.SERVER.receivePacket(handler.playerEntity, this);
         return null;
     }
 }

CleanroomMC/ModularUI

(cherry picked from commit c761c69b182417a4df5fadf1e6155727f612a440)

src/main/java/com/cleanroommc/modularui/ClientProxy.java
@@ -8,6 +8,7 @@ import com.cleanroommc.modularui.factory.inventory.InventoryTypes;
 import com.cleanroommc.modularui.holoui.HoloScreenEntity;
 import com.cleanroommc.modularui.holoui.ScreenEntityRender;
 import com.cleanroommc.modularui.keybind.KeyBindHandler;
+import com.cleanroommc.modularui.network.ModularNetwork;
 import com.cleanroommc.modularui.screen.ClientScreenHandler;
 import com.cleanroommc.modularui.test.TestItem;
 import com.cleanroommc.modularui.theme.ThemeManager;
@@ -21,6 +22,7 @@ import net.minecraft.util.Timer;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -179,6 +181,18 @@ public class ClientProxy extends CommonProxy {
                 }
                 return false;
             });
+        }
+    }
+
+    @SubscribeEvent
+    public void onUnloadWorld(WorldEvent.Unload event) {
+        if (Minecraft.getMinecraft().player != null) {
+            ModularNetwork.CLIENT.onPlayerLeave(Minecraft.getMinecraft().player);
+
+            if (Minecraft.getMinecraft().isIntegratedServerRunning()) {
+                // we need to handle single player here, since PlayerLoggedOutEvent is not triggered for some reason
+                ModularNetwork.SERVER.onPlayerLeave(Minecraft.getMinecraft().player);
+            }
         }
     }
 

src/main/java/com/cleanroommc/modularui/CommonProxy.java
@@ -4,7 +4,9 @@ import com.cleanroommc.modularui.factory.GuiFactories;
 import com.cleanroommc.modularui.factory.GuiManager;
 import com.cleanroommc.modularui.factory.inventory.InventoryTypes;
 import com.cleanroommc.modularui.holoui.HoloScreenEntity;
+import com.cleanroommc.modularui.network.ModularNetwork;
 import com.cleanroommc.modularui.network.NetworkHandler;
+import com.cleanroommc.modularui.network.NetworkUtils;
 import com.cleanroommc.modularui.screen.ModularContainer;
 import com.cleanroommc.modularui.test.ItemEditorGui;
 import com.cleanroommc.modularui.test.TestEventHandler;
@@ -19,6 +21,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
@@ -75,6 +78,13 @@ public class CommonProxy {
     public void onTick(TickEvent.PlayerTickEvent event) {
         if (event.player.openContainer instanceof ModularContainer container) {
             container.onUpdate();
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerLeave(PlayerEvent.PlayerLoggedOutEvent event) {
+        if (!NetworkUtils.isDedicatedClient()) {
+            ModularNetwork.SERVER.onPlayerLeave(event.player);
         }
     }
 }

src/main/java/com/cleanroommc/modularui/network/packets/PacketSyncHandler.java
@@ -4,6 +4,7 @@ import com.cleanroommc.modularui.network.IPacket;
 import com.cleanroommc.modularui.network.ModularNetwork;
 import com.cleanroommc.modularui.network.NetworkUtils;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.PacketBuffer;
@@ -53,13 +54,13 @@ public class PacketSyncHandler implements IPacket {
     @SideOnly(Side.CLIENT)
     @Override
     public @Nullable IPacket executeClient(NetHandlerPlayClient handler) {
-        ModularNetwork.CLIENT.receivePacket(this);
+        ModularNetwork.CLIENT.receivePacket(Minecraft.getMinecraft().player, this);
         return null;
     }
 
     @Override
     public @Nullable IPacket executeServer(NetHandlerPlayServer handler) {
-        ModularNetwork.SERVER.receivePacket(this);
+        ModularNetwork.SERVER.receivePacket(handler.player, this);
         return null;
     }
 }